619348425985304dc7b690b8f617a93f4251d9b2
rohit <rohitmahto062@gmail.com>
2026-02-16 22:57:26 +0530

    refactor: optimize insertion sort algorithm for improved performance

--- sorting/insertion-sort.js
+++ sorting/insertion-sort.js
@@ -1,15 +1,31 @@
+// Brute Force
+// function insertionSort(arr) {
+//   let n = arr.length;
+//   for (let j = 1; j < n; j++) {
+//     let curr = j;
+//     for (let i = curr - 1; i >= 0; i--) {
+//       if (arr[i] > arr[curr]) {
+//         let temp = arr[i];
+//         arr[i] = arr[curr];
+//         arr[curr] = temp;
+//         curr--;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// Optimise
 function insertionSort(arr) {
   let n = arr.length;
-  for (let j = 0; j < n; j++) {
-    let curr = j;
-    for (let i = curr - 1; i >= 0; i--) {
-      if (arr[i] > arr[curr]) {
-        let temp = arr[i];
-        arr[i] = arr[curr];
-        arr[curr] = temp;
-        curr--;
-      }
+  for (let i = 1; i < n; i++) {
+    let curr = arr[i];
+    let j = i - 1;
+    while ((j >= 0) & (arr[j] > curr)) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = curr;
   }
   return arr;
 }
