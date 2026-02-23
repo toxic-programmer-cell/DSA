a0dc1c32a376b6767dbed9be2860d124b6568e46
rohit <rohitmahto062@gmail.com>
2026-02-23 12:36:25 +0530

    add: implement basic structure for linked list with Node and MyLinkedList classes

---
+++ Linked-list/create-linked-list/index.js
@@ -0,0 +1,11 @@
+// Representation of node(Created Node)
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+
+// Created Linked List
+let MyLinkedList = function () {
+  this.head = null;
+  this.size = 0;
+};
